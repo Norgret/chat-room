13b374ff2b00a317895025a2f2c4d644366e9bfb
coding-planet-X <parker.michalenko4256@stu.mtec.edu>
2021-09-13 09:22:02 -0600

    index page

--- js/temp.js
+++ js/temp.js
@@ -1,5 +1,5 @@
 
-$(".fas").click(function () {
+$(".fas").on("click",function () {
     $(this).parent().parent().toggleClass("background");
     $(this).parent().next().toggleClass("hidden");
     $(this).parent().toggleClass("tansformers");
